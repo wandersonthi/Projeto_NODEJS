b507f0c701180f8c015dd635b36fdb4d85d961ae
wandersonthi <82480076+wandersonthi@users.noreply.github.com>
2024-03-25 18:43:12 -0300

    Create insert.js

---
+++ insert.js
@@ -0,0 +1,16 @@
+const mongodb=require(mongodb).MongoClient
+const url="mongodb+srv://wandersonthi:<10Demaiovida>@cluster0.1g0onws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+
+mongodb.conect(url, (erro,banco)=>{
+    if(erro)throw erro;
+    const dbo=banco.db("wrcursos")
+    const obj={curso:" curso de Node", sala:"sites dinamicos"}
+    const colecao="cursos"
+    Boolean.collection(colecao).insertOne(obj,(erro,resultato)=>{
+        if(erro)throw erro
+        console.log("1 novo curso inserido")
+        banco.close()
+
+
+    })
+})
