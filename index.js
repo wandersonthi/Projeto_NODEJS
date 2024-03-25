bd9267ef2290b55f5c7d25c7b2b52ecc5155df0b
wandersonthi <82480076+wandersonthi@users.noreply.github.com>
2024-03-25 10:26:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 
-mongoose.connect('urldomongo ou do local host',{
+mongoose.connect('mongodb+srv://wandersonthi:<10Demaio>@cluster0.fz4ytuw.mongodb.net/',{
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
