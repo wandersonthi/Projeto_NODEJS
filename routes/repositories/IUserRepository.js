874ec4fa3f30694bd286f5e0810f63f6e4401e8d
wandersonthi <82480076+wandersonthi@users.noreply.github.com>
2024-04-08 18:23:43 -0300

    Add files via upload

---
+++ routes/repositories/IUserRepository.js
@@ -0,0 +1,7 @@
+// repositories/IUserRepository.js
+module.exports = class IUserRepository {
+    create(user) {
+        throw new Error('You have to implement the method create!');
+    }
+    // Defina outros métodos necessários
+}
---
+++ routes/repositories/UserRepository.js
@@ -0,0 +1,10 @@
+// repositories/UserRepository.js
+const IUserRepository = require('./IUserRepository');
+const User = require('../models/User'); // Supondo que você tenha um modelo de usuário
+
+module.exports = class UserRepository extends IUserRepository {
+    create(user) {
+        return User.create(user);
+    }
+    // Implemente outros métodos necessários
+}
